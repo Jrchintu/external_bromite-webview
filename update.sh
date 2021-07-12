454d420e4cabd5de949df8d66a946dcac11fa2b2
CHINTU <harrypetersun@outlook.com>
2021-07-12 22:00:47 +0530

    Add credentials

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+## CONFIG
+git config --global user.name "jrchintu"
+git config --global user.email "harrypetersun@gmail.com"
+git config --global credential.helper store
+echo "https://jrchintu:$GIT_PASS@github.com" > ~/.git-credentials
+
 git clone --depth=1 https://github.com/Jrchintu/external_bromite-webview.git -b R11 DEADDIR && cd DEADDIR || exit
 curl -s https://api.github.com/repos/bromite/bromite/releases/latest \
 | grep "_SystemWebView\\.apk\|brm.*txt" | cut -d : -f 2,3 | tr -d \" | wget -qi -
